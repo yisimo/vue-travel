afcc562bcbb8e02466f160ec580e05c55125ce1c
xiequan <xiequan0759@163.com>
2019-03-09 12:33:45 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 引入border.css解决不同移动端屏幕1像素显示的问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 引入fastClick解决部分移动端click事件延迟300毫秒执行的问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  color: red;
+}
+</style>
